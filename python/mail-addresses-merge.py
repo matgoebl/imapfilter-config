4cf4c780a20b3cc3379f1a0524544f7fbf3b1348
Matthias Goebl <matthias.goebl@goebl.net>
2023-01-01 20:47:46 +0100

    add option to exclude existing email addresses

--- python/mail-addresses-merge.py
+++ python/mail-addresses-merge.py
@@ -3,15 +3,19 @@ import mailbox
 import click
 import sys
 import re
+import glob
 
 @click.command()
 @click.option('-f', '--file',    help='File with eMail addresses to read and write back.', type=click.File('r+'))
 @click.option('-a', '--add',     help='File with addresses to add (default: stdin)', type=click.File('r'), default='-')
 @click.option('-x', '--exclude', help='Exclude email addresses matching given regex.')
+@click.option('-e', '--existing',help='Exclude existing email addresses found in files matching the given file glob (e.g. lists/*.senders).')
 @click.option('-v', '--verbose', help="Be verbose.", count=True)
-def mail_addresses_merge(file, add, exclude, verbose):
+def mail_addresses_merge(file, add, exclude, existing, verbose):
     """Merge and normalize list with eMail addresses."""
-    emails = EmailCollection(file.readlines(), exclude, verbose)
+    emails = EmailCollection(file.readlines(), exclude, existing, verbose)
+    if verbose:
+        print(f"* merging to {file.name}:")
     for line in add.readlines():
         if verbose:
             print(f"{line}")
@@ -23,13 +27,23 @@ def mail_addresses_merge(file, add, exclude, verbose):
 
 
 class EmailCollection():
-    def __init__(self, emails=[], exclude=None, verbose=False):
+    def __init__(self, emails=[], exclude=None, existing=None, verbose=False):
         self.emails = [ email.strip().lower() for email in emails ]
         self.exclude = None
         if exclude:
             self.exclude = re.compile(exclude)
         self.verbose = verbose
-        self.regex = re.compile(r'([-._A-Za-z0-9]+@[-.A-Za-z0-9]+)')
+        self.validemail = re.compile(r'([-._A-Za-z0-9]+@[-.A-Za-z0-9]+)')
+        self.existing = []
+        if existing:
+            for filename in sorted(glob.glob(existing)):
+                if self.verbose:
+                    print(f"* reading existing {filename}...")
+                with open(filename) as f:
+                    self.existing += [ email.strip().lower() for email in f.readlines() ]
+        if self.verbose:
+            print(f"* list of existing addresses:\n{self.existing}")
+
     def add(self, emails):
     	if not emails:
             return
@@ -38,12 +52,14 @@ class EmailCollection():
     def add_one(self, email):
         if not email:
             return
-        match = self.regex.search(email)
+        match = self.validemail.search(email)
         if match:
             addr = match.group(1)
             if addr:
                 addr = addr.lower()
                 if self.exclude and self.exclude.match(addr):
+                    return
+                if addr in self.existing:
                     return
                 self.emails.append(addr)
                 if self.verbose:
@@ -51,8 +67,7 @@ class EmailCollection():
     def get(self):
         self.emails = [ email.strip().lower() for email in self.emails if email != "" and "@" in email ]
         lst = list(set(self.emails))
-        #lst.sort()
-        lst = sorted(lst, key=lambda x: x.split('@')[1])
+        lst = sorted(lst, key=lambda x: x.split('@')[1] + "@" + x.split('@')[0])
         return lst
 
 
