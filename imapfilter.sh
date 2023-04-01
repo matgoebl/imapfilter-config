250fc69d2f21df2746f57e49585b29a1764fe62a
Matthias Goebl <matthias.goebl@goebl.net>
2023-04-01 15:59:34 +0200

    start in dry-run and debug mode

--- imapfilter.sh
+++ imapfilter.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 export LUA_PATH="$PWD/lua/?.lua;$PWD/?.lua"
+
 pkill --uid `id -un` --exact --echo imapfilter
-imapfilter -c lua/imapfilter.lua -l out.log -n "$@"
+rm -f *.log
+
+if [ "$1" = "-n" ]; then  # dry-run
+ DEBUG=y DRYRUN=y imapfilter -c lua/imapfilter.lua
+elif [ "$1" = "-d" ]; then  # debug
+ DEBUG=y imapfilter -c lua/imapfilter.lua -l err.log 2>&1 | tee out.log
+else  # normal
+ DEBUGLOGFILE=debug.log imapfilter -c lua/imapfilter.lua -l err.log
+ sleep 1
+ tail -n +0 -f debug.log
+fi
